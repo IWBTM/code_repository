8908c67a88185c3c276252a60f24bd358533fcb3
김경민(화려한 덕후들) <gang00@g-oduk.com>
2023-09-04 15:23:55 +0900

    백준 - 단계별로 풀어보기 - 기하 - 직사각형

--- src/main/java/backjoon/기하/Main.java
+++ src/main/java/backjoon/기하/Main.java
@@ -1,7 +1,24 @@
 package backjoon.기하;
 
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        String result = String.valueOf(A * B);
+        bw.write(result);
+
+        bw.flush();
+        closeBuffer(br, bw);
+    }
+
+    public static void closeBuffer(BufferedReader br, BufferedWriter bw) throws IOException {
+        bw.close();
+        br.close();
     }
 }
